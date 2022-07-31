60d8a73d2c027e93bd5996db14f64f9aeabeb9cb
Matvey <adntipin.m9971@gmail.com>
2022-07-31 11:20:51 +0300

    modify index.html

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,21 +1,3 @@
-/* window.addEventListener('DOMContentLoaded', () => {
-	const menu = document.querySelector('.menu'),
-		menuItem = document.querySelectorAll('.menu_item'),
-		hamburger = document.querySelector('.hamburger');
-
-	hamburger.addEventListener('click', () => {
-		hamburger.classList.toggle('hamburger_active');
-		menu.classList.toggle('menu_active');
-	});
-
-	menuItem.forEach(item => {
-		item.addEventListener('click', () => {
-			hamburger.classList.toggle('hamburger_active');
-			menu.classList.toggle('menu_active');
-		})
-	})
-}) */
-
 const hamburger = document.querySelector('.hamburger'),
     menu = document.querySelector('.menu'),
     closeElem = document.querySelector('.menu__close'),
